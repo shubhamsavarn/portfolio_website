b0142392af92f04ab71c1e3a4eaaee1871685ad2
shubhamsavarn <shubham.savarn99@gmail.com>
2022-07-31 01:37:21 +0530

    home change

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,42 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
-/*import AnimatedLetters from '../AnimatedLetters'
+/*import AnimatedLetters from '../AnimatedLetters'*/
 import LogoTitle from '../../assets/images/logo-s.png'
-import Logo from './Logo'*/
+import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
+
   return (
     <>
+      <div className="container home-page">
+      <div className="text-zone">
+      <h1>
+            <span >H</span>
+            <span >i,</span>
+            <br />
+            <span >I</span>
+            <span>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            hubham 
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            avarn
+            <br/>
+            web developer
+      </h1>
+      <h2>Front End Developer / JavaScript Expert </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+      </div>
+      </div>
     </>
   )
 }

1ddbacb6dc5be77075e2d5957012b61cb952dffe
shubhamsavarn <shubham.savarn99@gmail.com>
2022-07-31 02:03:07 +0530

    logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,41 +1,64 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
-/*import AnimatedLetters from '../AnimatedLetters'*/
+import AnimatedLetters from '../AnimatedLetters'
 import LogoTitle from '../../assets/images/logo-s.png'
 import Logo from './Logo'
 import './index.scss'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [ 'h', 'u', 'b', 'h', 'a', 'm']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
 
   return (
     <>
       <div className="container home-page">
       <div className="text-zone">
       <h1>
-            <span >H</span>
-            <span >i,</span>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
             <br />
-            <span >I</span>
-            <span>'m</span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
             <img
               src={LogoTitle}
               alt="JavaScript Developer Name, Web Developer Name"
             />
-            hubham 
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
             />
-            avarn
-            <br/>
-            web developer
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
       </h1>
       <h2>Front End Developer / JavaScript Expert </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
       </div>
+      <Logo />
       </div>
     </>
   )
